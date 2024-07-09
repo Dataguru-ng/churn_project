b9a62794a8d152fbdd03d67f1a0fa631818e1471
Dataguru-ng <deborahobeke@gmail.com>
2024-07-09 19:21:48 +0100

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
 import pypickle
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn import preprocessing
